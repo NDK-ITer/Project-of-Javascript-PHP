b5547202b00329a7de19a0cc72a5994659de3cac
skyred123 <tn29805@gmail.com>
2024-04-14 05:06:50 +0700

    update Sidebar

--- FE/web_admin/lib/home.dart
+++ FE/web_admin/lib/home.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:web_admin/core/color.dart';
 import 'package:web_admin/widgets/sidebar.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,18 +11,121 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String content = 'Home';
+
+  void loadPage(String data) {
+    setState(() {
+      content = data;
+    });
+  }
+
+  final List<Map<String, dynamic>> _sidebarItems = [
+    {
+      'text': 'User Management',
+      'icon': Icons.people,
+      'selected': false,
+      'items': [
+        {
+          'text': 'Account',
+          'selected': false,
+        },
+        {
+          'text': 'Role',
+          'selected': false,
+        }
+      ]
+    },
+    {
+      'text': 'Job Management',
+      'icon': Icons.work,
+      'selected': false,
+      'items': [
+        {
+          'text': 'Employer',
+          'selected': false,
+        },
+        {
+          'text': 'Detail Recruitment',
+          'selected': false,
+        }
+      ]
+    },
+    {
+      'text': 'Applicant Management',
+      'icon': Icons.school,
+      'selected': false,
+      'items': [
+        {
+          'text': 'Employee',
+          'selected': false,
+        },
+        {
+          'text': 'Recruitment Article',
+          'selected': false,
+        },
+      ]
+    },
+    {
+      'text': 'Reporting',
+      'icon': Icons.bar_chart,
+      'selected': false,
+      'items': [
+        {
+          'text': 'Enjoy',
+          'selected': false,
+        }
+      ]
+    },
+    {
+      'text': 'System Configuration',
+      'icon': Icons.system_update_tv,
+      'selected': false,
+      'items': [
+        {
+          'text': 'Field',
+          'selected': false,
+        }
+      ]
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
+      backgroundColor: AppColors.neutralColor1,
       // appBar: AppBar(
       //   title: Text('Home'),
       // ),
       body: Center(
         child: Row(
           children: [
-            Expanded(flex: 1, child: SidebarWidget()),
-            Expanded(flex: 5, child: Container()),
+            SizedBox(
+              width: 320,
+              child: SidebarWidget(
+                onItemTap: loadPage,
+                logo: SvgPicture.asset(
+                  'assets/svg/logo_icon.svg',
+                  width: 30,
+                  height: 30,
+                  fit: BoxFit.fill,
+                ),
+                title: 'WhatJob',
+                sidebarItems: _sidebarItems,
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: SizedBox(
+                child: Container(
+                  child: Center(
+                    child: Text(content,
+                        style: TextStyle(
+                            fontSize: 20, color: AppColors.neutralColor8)),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
--- FE/web_admin/lib/widgets/sidebar.dart
+++ FE/web_admin/lib/widgets/sidebar.dart
@@ -1,11 +1,22 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:web_admin/core/color.dart';
 
 class SidebarWidget extends StatefulWidget {
-  const SidebarWidget({super.key});
+  final Function(String data) onItemTap;
+  final Widget logo;
+  final String title;
+  final List<Map<String, dynamic>> sidebarItems;
+  const SidebarWidget(
+      {super.key,
+      required this.onItemTap,
+      required this.logo,
+      required this.title,
+      required this.sidebarItems});
 
   @override
   // ignore: library_private_types_in_public_api
@@ -13,37 +24,61 @@ class SidebarWidget extends StatefulWidget {
 }
 
 class _SidebarWidgetState extends State<SidebarWidget> {
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
+  void _onItemTapped({int? index_parent, int index_child = -1}) {
     setState(() {
-      _selectedIndex = index;
+      for (int i = 0; i < widget.sidebarItems.length; i++) {
+        if (i == index_parent && index_child == -1) {
+          widget.sidebarItems[i]['selected'] =
+              !widget.sidebarItems[i]['selected'];
+        } else if (index_child == -1) {
+          widget.sidebarItems[i]['selected'] = false;
+        }
+        for (int j = 0; j < widget.sidebarItems[i]['items'].length; j++) {
+          if (j == index_child) {
+            widget.sidebarItems[i]['items'][j]['selected'] =
+                !widget.sidebarItems[i]['items'][j]['selected'];
+          } else {
+            widget.sidebarItems[i]['items'][j]['selected'] = false;
+          }
+        }
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: AppColors.neutralColor1,
+      decoration: BoxDecoration(
+        color: AppColors.neutralColor1,
+        border: Border(
+          right: BorderSide(
+            color: AppColors.neutralColor2,
+            width: 1,
+            style: BorderStyle.solid,
+          ),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.2),
+            spreadRadius: 2,
+            blurRadius: 5,
+          ),
+        ],
+      ),
       child: Column(
         children: [
           SizedBox(height: 25),
           Container(
             child: Padding(
-              padding: const EdgeInsets.only(left: 30),
+              padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Row(
                 children: [
-                  SvgPicture.asset(
-                    'assets/svg/logo_icon.svg',
-                    width: 30,
-                    height: 30,
-                    fit: BoxFit.fill,
-                  ),
+                  widget.logo,
                   SizedBox(
                     width: 10,
                   ),
                   Text(
-                    'WhatJob',
+                    widget.title,
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
                         color: AppColors.neutralColor8,
@@ -55,42 +90,182 @@ class _SidebarWidgetState extends State<SidebarWidget> {
           ),
           SizedBox(height: 32),
           Expanded(
-            child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  selected: _selectedIndex == index,
-                  selectedColor: Colors.white,
-                  onTap: () => _onItemTapped(index),
-                  leading: Icon(
-                    index == 0
-                        ? Icons.home
-                        : index == 1
-                            ? Icons.person
-                            : index == 2
-                                ? Icons.settings
-                                : index == 3
-                                    ? Icons.notifications
-                                    : Icons.exit_to_app,
-                    color: Colors.white,
-                  ),
-                  title: Text(
-                    index == 0
-                        ? 'Home'
-                        : index == 1
-                            ? 'Profile'
-                            : index == 2
-                                ? 'Settings'
-                                : index == 3
-                                    ? 'Notifications'
-                                    : 'Logout',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                for (int i = 0; i < widget.sidebarItems.length; i++) ...[
+                  GestureDetector(
+                    onTap: () {
+                      _onItemTapped(index_parent: i);
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 10),
+                                    child: Icon(
+                                      widget.sidebarItems[i]['icon'],
+                                      color: widget.sidebarItems[i]['selected']
+                                          ? AppColors.primaryColor1
+                                          : AppColors.neutralColor5,
+                                      size: 20,
+                                    ),
+                                  ),
+                                  SizedBox(width: 10),
+                                  Text(widget.sidebarItems[i]['text'],
+                                      style: TextStyle(
+                                          color: widget.sidebarItems[i]
+                                                  ['selected']
+                                              ? AppColors.primaryColor1
+                                              : AppColors.neutralColor5,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w800)),
+                                ],
+                              ),
+                              Padding(
+                                padding: EdgeInsets.symmetric(vertical: 10),
+                                child: Icon(
+                                  widget.sidebarItems[i]['selected']
+                                      ? Icons.keyboard_arrow_down
+                                      : Icons.navigate_next,
+                                  color: AppColors.neutralColor5,
+                                  size: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                          if (widget.sidebarItems[i]['selected']) ...[
+                            for (int j = 0;
+                                j < widget.sidebarItems[i]['items'].length;
+                                j++) ...[
+                              GestureDetector(
+                                onTap: () {
+                                  widget.onItemTap(widget.sidebarItems[i]
+                                      ['items'][j]['text']);
+                                  _onItemTapped(
+                                      index_parent: i, index_child: j);
+                                },
+                                child: Container(
+                                  margin: EdgeInsets.symmetric(vertical: 3),
+                                  child: Row(
+                                    children: [
+                                      Container(
+                                        width: 5,
+                                        height: 40,
+                                        decoration: ShapeDecoration(
+                                          color: widget.sidebarItems[i]['items']
+                                                  [j]['selected']
+                                              ? AppColors.primaryColor1
+                                              : AppColors.neutralColor1,
+                                          shape: RoundedRectangleBorder(
+                                            side: BorderSide(
+                                                width: 0.60,
+                                                color: widget.sidebarItems[i]
+                                                        ['items'][j]['selected']
+                                                    ? AppColors.primaryColor1
+                                                    : AppColors.neutralColor1),
+                                            borderRadius: BorderRadius.only(
+                                                topLeft: Radius.circular(4),
+                                                bottomLeft: Radius.circular(4)),
+                                          ),
+                                        ),
+                                      ),
+                                      Expanded(
+                                        child: Container(
+                                          decoration: ShapeDecoration(
+                                            color: widget.sidebarItems[i]
+                                                    ['items'][j]['selected']
+                                                ? AppColors.neutralColor3
+                                                : AppColors.neutralColor1,
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius: BorderRadius.only(
+                                                  topRight: Radius.circular(4),
+                                                  bottomRight:
+                                                      Radius.circular(4)),
+                                            ),
+                                          ),
+                                          padding: const EdgeInsets.symmetric(
+                                              vertical: 10.0),
+                                          child: Row(
+                                            children: [
+                                              SizedBox(width: 10),
+                                              Text(
+                                                  widget.sidebarItems[i]
+                                                      ['items'][j]['text'],
+                                                  style: TextStyle(
+                                                      color: widget.sidebarItems[
+                                                                  i]['items'][j]
+                                                              ['selected']
+                                                          ? AppColors
+                                                              .neutralColor8
+                                                          : AppColors
+                                                              .neutralColor5,
+                                                      fontSize: 16,
+                                                      fontWeight:
+                                                          FontWeight.w800)),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ]
+                          ]
+                        ],
+                      ),
                     ),
                   ),
-                );
-              },
+                  SizedBox(height: 10),
+                ],
+                Divider(
+                  color: AppColors.neutralColor4,
+                  thickness: 1,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, top: 15, bottom: 15),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.settings,
+                        color: AppColors.neutralColor5,
+                        size: 20,
+                      ),
+                      SizedBox(width: 10),
+                      Text('Settings',
+                          style: TextStyle(
+                            color: AppColors.neutralColor5,
+                            fontSize: 14,
+                          )),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30, top: 15, bottom: 15),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.exit_to_app,
+                        color: AppColors.neutralColor5,
+                        size: 20,
+                      ),
+                      SizedBox(width: 10),
+                      Text('Logout',
+                          style: TextStyle(
+                            color: AppColors.neutralColor5,
+                            fontSize: 14,
+                          )),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
         ],
